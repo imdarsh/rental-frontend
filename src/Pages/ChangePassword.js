c13bc38614f5b186847cab121ea5d0fa50bbbd7a
imdarsh <shindesahil63@gmail.com>
2022-03-31 00:21:48 +0530

    Worked on Change Password

--- src/Pages/ChangePassword.js
+++ src/Pages/ChangePassword.js
@@ -1,7 +1,7 @@
 import { Button, Container, Divider, FormControl, TextField, Typography, Alert } from '@mui/material';
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Navbar from '../Components/Navbar';
 import Loading from '../Components/Loading';
@@ -12,24 +12,27 @@ function ChangePassword() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const changePassword = async () => {
+    let navigate = useNavigate();
+
+    const changePassword = async (e) => {
+        e.preventDefault();
         setLoading(true);
         if(user.newpassword === user.cnfnewpassword) {
             const users = {
                 oldpassword: user.oldpassword,
-                password: user.newpassword
+                newpassword: user.newpassword
             }
             let id = JSON.parse(localStorage.getItem('user')).userId; 
             await axios.patch(`http://localhost:4000/api/v1/change-password/${id}`, users)
             .then(function (response) {
                 console.log(true);
                 setLoading(false);
-                Navigate(`/my-profile`);
+                navigate(`/my-profile`);
             })
             .catch(function (error) {
                 setLoading(false);
+                
                 setError(error.response.data.message);
-
             })
         }
         else {
@@ -50,17 +53,17 @@ function ChangePassword() {
             <Typography textAlign="center" variant="h5" fontWeight="bold" sx={{ my:2 }}>Change Password</Typography>
             <Container>
                 <Divider sx={{ background: 'black' }} />
-                { error && <Alert severity="error">{error}</Alert>}      
+                { error && <Alert sx={{ my:2 }} severity="error">{error}</Alert>}      
                 <Typography variant="h5" sx={{textAlign:'center'}}>{ loading && <Loading />}</Typography>
                 <form onSubmit={changePassword} id="change-password-form">
                 <FormControl sx={{ my:2 }} fullWidth>
-                    <TextField type="password" size="small" label="Old Password" id="oldpassword" onChange={handleChange} value={user.oldpassword} />
+                    <TextField type="password" size="small" label="Old Password" name="oldpassword" id="oldpassword" onChange={handleChange} value={user.oldpassword} />
                 </FormControl>
                 <FormControl sx={{ my:2 }} fullWidth>
-                    <TextField type="password" size="small" label="New Password" id="newpassword" onChange={handleChange} value={user.newpassword} />
+                    <TextField type="password" size="small" label="New Password" name="newpassword" id="newpassword" onChange={handleChange} value={user.newpassword} />
                 </FormControl>
                 <FormControl sx={{ my:2 }} fullWidth>
-                    <TextField type="password" size="small" label="Confirm New Password" id="cnfnewpassword" onChange={handleChange} value={user.cnfnewpassword} />
+                    <TextField type="password" size="small" label="Confirm New Password" name="cnfnewpassword" id="cnfnewpassword" onChange={handleChange} value={user.cnfnewpassword} />
                 </FormControl>
                 <Button type="submit" variant="contained" sx={{ my:2 }}>Change Password</Button>
                 </form>
